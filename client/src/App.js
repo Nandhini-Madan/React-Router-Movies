e7e0861d35c91b26e41a0aaaa467c85610cfd98b
unknown <nandhinimadan20@gmail.com>
2020-07-08 23:47:36 -0700

    task2 partially completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, Link ,Switch} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -13,6 +15,7 @@ const App = () => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovieList(response.data);
+          console.log("mov",response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -27,8 +30,21 @@ const App = () => {
 
   return (
     <div>
+      <nav>
+        <Link to="/MovieList">MovieList</Link>
+      </nav>
+      {console.log("movieList",movieList)}
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+      <Route path="/MovieList">
+        <MovieList movies={movieList}/>
+       </Route>
+       <Route path="/MovieList/:id">
+         <Movie />
+       </Route>
+      </Switch>
+      
+      
     </div>
   );
 };
